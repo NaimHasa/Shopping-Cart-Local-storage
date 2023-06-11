94dcc7b1656ac5a5020577752a79370735b67e6b
Naim Hasan <naimarafat7@gmail.com>
2023-06-11 00:23:06 -0700

    simple local storage

--- Js/shoppingCart.js
+++ Js/shoppingCart.js
@@ -12,6 +12,23 @@ const getInputvalueByID = id => {
 const addProduct = () => {
     const product = getInputvalueByID('product-name');
     const quantity = getInputvalueByID('product-quntity');
+    displayDataShow(product, quantity);
     console.log(product, quantity);
+
+
+    //add to local storage 
+
+    //simple way
+
+    localStorage.setItem(product, quantity);
+
+
+}
+const displayDataShow = (product, quantity) => {
+    const productContainer = document.getElementById('product-container');
+    const li = document.createElement('li')
+    li.innerText = ` ${product} : ${quantity}
+    `;
+    productContainer.appendChild(li);
 }
 
